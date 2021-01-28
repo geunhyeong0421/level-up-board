3c91b3c3338457b4674ef3bb8f0bb386d314d9df
geunhyeong0421 <geunhyeong0421@gmail.com>
2021-01-28 22:58:43 +0900

    프로젝트 생성

---
+++ src/main/java/com/gh/levelupboard/LevelUpBoardApplication.java
@@ -0,0 +1,13 @@
+package com.gh.levelupboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LevelUpBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LevelUpBoardApplication.class, args);
+	}
+
+}
