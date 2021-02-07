7bc7cf0a5a43991d57e9cd62774e04d4b7a3b374
geunhyeong0421 <geunhyeong0421@gmail.com>
2021-02-08 03:34:32 +0900

    Lv.5 - 테스트 코드 수정 및 테스트 버전 배포(메모리 DB)

--- src/test/java/com/gh/levelupboard/domain/post/PostRepositoryTest.java
+++ src/test/java/com/gh/levelupboard/domain/post/PostRepositoryTest.java
@@ -1,33 +1,81 @@
 package com.gh.levelupboard.domain.post;
 
-import org.assertj.core.api.Assertions;
+import com.gh.levelupboard.domain.user.LoginType;
+import com.gh.levelupboard.domain.user.Role;
+import com.gh.levelupboard.domain.user.User;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
+import javax.annotation.PostConstruct;
+import javax.persistence.EntityManager;
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.*;
+
+@Transactional
 @SpringBootTest
 class PostRepositoryTest {
 
     @Autowired
     PostRepository postRepository;
+    @Autowired
+    EntityManager em;
 
-//    @Test
-//    public void 게시글저장_불러오기() throws Exception {
-//        //given
-//        String title = "테스트 게시글";
-//        String content = "테스트 본문";
-//
-//        postRepository.save(new Post(title, content, "geunhyeong0421@gmail.com"));
-//
-//        //when
-//        List<Post> postList = postRepository.findAll();
-//
-//        //then
-//        Post post = postList.get(0);
-//        Assertions.assertThat(post.getTitle()).isEqualTo(title);
-//        Assertions.assertThat(post.getContent()).isEqualTo(content);
-//    }
+    User user;
+
+    @PostConstruct
+    public void setup() {
+        user = User.builder()
+                .loginType(LoginType.GOOGLE)
+                .loginId("1234")
+                .name("test")
+                .email("test@gmail.com")
+                .picture(null)
+                .role(Role.USER)
+                .build();
+    }
+
+
+    @Test
+    public void saveTest() throws Exception {
+    /**
+     * GenerationType.IDENTITY 설정으로 인해 persist()와 동시에 쿼리가 날라간다..
+     */
+        //given
+        em.persist(user); // 영속성 컨텍스트에 포함되면서 pk인 id값 생성과 insert 쿼리 발생
+        String title = "title";
+        String content = "content";
+
+        //when
+        Post savedPost = postRepository.save(new Post(title, content, user));
+
+        //then
+        assertThat(savedPost.getTitle()).isEqualTo(title);
+        assertThat(savedPost.getContent()).isEqualTo(content);
+    }
+
+
+    @Test
+    public void findAllDescTest() throws Exception {
+    /**
+     * GenerationType.IDENTITY 설정으로 인해 persist()와 동시에 쿼리가 날라간다..
+     */
+        //given
+        em.persist(user);
+        postRepository.save(new Post("title1", "content1", user));
+        postRepository.save(new Post("title2", "content2", user));
+        postRepository.save(new Post("title3", "content3", user));
+
+        //when
+        List<Post> postList = postRepository.findAllDesc(); // 등록 순서의 역순으로 조회
+
+        //then
+        assertThat(postList)
+                .extracting("title")
+                .containsExactly("title3", "title2", "title1");
+    }
+
 
 }
--- src/test/java/com/gh/levelupboard/web/PostApiControllerTest.java
+++ src/test/java/com/gh/levelupboard/web/PostApiControllerTest.java
@@ -3,6 +3,10 @@ package com.gh.levelupboard.web;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.gh.levelupboard.domain.post.Post;
 import com.gh.levelupboard.domain.post.PostRepository;
+import com.gh.levelupboard.domain.user.LoginType;
+import com.gh.levelupboard.domain.user.Role;
+import com.gh.levelupboard.domain.user.User;
+import com.gh.levelupboard.domain.user.UserRepository;
 import com.gh.levelupboard.web.post.dto.PostSaveRequestDto;
 import com.gh.levelupboard.web.post.dto.PostUpdateRequestDto;
 import org.junit.jupiter.api.AfterEach;
@@ -10,7 +14,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.*;
 import org.springframework.security.test.context.support.WithMockUser;
@@ -19,11 +22,11 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
 import java.util.List;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.*;
 import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -33,20 +36,15 @@ class PostApiControllerTest {
     private int port;
 
     @Autowired
-    private TestRestTemplate restTemplate;
-
-    @Autowired
     private PostRepository postRepository;
-
-    @AfterEach
-    public void tearDown() throws Exception {
-        postRepository.deleteAll();
-    }
+    @Autowired
+    private UserRepository userRepository;
 
     @Autowired
     private WebApplicationContext context;
 
     private MockMvc mvc;
+    private User savedUser;
 
     @BeforeEach
     public void setup() {
@@ -54,69 +52,89 @@ class PostApiControllerTest {
                 .webAppContextSetup(context)
                 .apply(springSecurity())
                 .build();
+
+        User user = User.builder()
+                .loginType(LoginType.GOOGLE)
+                .loginId("1234")
+                .name("test")
+                .email("test@gmail.com")
+                .picture(null)
+                .role(Role.USER)
+                .build();
+        savedUser = userRepository.save(user);
+    }
+
+    @AfterEach
+    public void tearDown() throws Exception {
+        postRepository.deleteAll();
     }
 
 
-//    @Test
-//    @WithMockUser(roles = "USER")
-//    public void Post_등록된다() throws Exception {
-//        //given
-//        String title = "title";
-//        String content = "content";
-//        PostSaveRequestDto requestDto = new PostSaveRequestDto(title, content, "writer");
-//
-//        String url = "http://localhost:" + port + "/api/v1/posts";
-//
-//        //when
-////        ResponseEntity<Long> responseEntity = restTemplate.postForEntity(url, requestDto, Long.class);
-//
-//        mvc.perform(post(url)
-//                .contentType(MediaType.APPLICATION_JSON)
-//                .content(new ObjectMapper().writeValueAsString(requestDto)))
-//                .andExpect(status().isOk());
-//
-//        //then
-////        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
-////        assertThat(responseEntity.getBody()).isGreaterThan(0L);
-//
-//        List<Post> all = postRepository.findAll();
-//        assertThat(all.get(0).getTitle()).isEqualTo(title);
-//        assertThat(all.get(0).getContent()).isEqualTo(content);
-//    }
-//
-//
-//    @Test
-//    @WithMockUser(roles = "USER")
-//    public void Post_수정된다() throws Exception {
-//        //given
-//        Post savedPost = postRepository.save(new Post("title", "content", "writer"));
-//
-//        Long updateId = savedPost.getId();
-//        String expectedTitle = "title2";
-//        String expectedContent = "content2";
-//
-//        PostUpdateRequestDto requestDto = new PostUpdateRequestDto(expectedTitle, expectedContent);
-//
-//        String url = "http://localhost:" + port + "/api/v1/posts/" + updateId;
-//
-////        HttpEntity<PostUpdateRequestDto> requestEntity = new HttpEntity<>(requestDto);
-//
-//        //when
-////        ResponseEntity<Long> responseEntity = restTemplate.exchange(url, HttpMethod.PUT, requestEntity, Long.class);
-//
-//        mvc.perform(put(url)
-//                .contentType(MediaType.APPLICATION_JSON)
-//                .content(new ObjectMapper().writeValueAsString(requestDto)))
-//                .andExpect(status().isOk());
-//
-//        //then
-////        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
-////        assertThat(responseEntity.getBody()).isGreaterThan(0L);
-//
-//        List<Post> all = postRepository.findAll();
-//        assertThat(all.get(0).getTitle()).isEqualTo(expectedTitle);
-//        assertThat(all.get(0).getContent()).isEqualTo(expectedContent);
-//    }
+    @Test
+    @WithMockUser(roles = "USER")
+    public void Post_등록된다() throws Exception {
+        //given
+        String title = "title";
+        String content = "content";
+        PostSaveRequestDto requestDto = new PostSaveRequestDto(title, content, savedUser.getId());
+
+        String url = "http://localhost:" + port + "/api/v1/posts";
+
+        //when
+        mvc.perform(post(url)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(new ObjectMapper().writeValueAsString(requestDto)))
+                .andExpect(status().isOk());
+
+        //then
+        List<Post> all = postRepository.findAll();
+        assertThat(all.get(0).getTitle()).isEqualTo(title);
+        assertThat(all.get(0).getContent()).isEqualTo(content);
+    }
+
+    @Test
+    @WithMockUser(roles = "USER")
+    public void Post_수정된다() throws Exception {
+        //given
+        Post savedPost = postRepository.save(new Post("title", "content", savedUser));
+        Long postId = savedPost.getId();
+
+        String expectedTitle = "title2";
+        String expectedContent = "content2";
+
+        PostUpdateRequestDto requestDto = new PostUpdateRequestDto(expectedTitle, expectedContent);
+
+        String url = "http://localhost:" + port + "/api/v1/posts/" + postId;
+
+        //when
+        mvc.perform(put(url)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(new ObjectMapper().writeValueAsString(requestDto)))
+                .andExpect(status().isOk());
+
+        //then
+        Post post = postRepository.findById(postId).get();
+        assertThat(post.getTitle()).isEqualTo(expectedTitle);
+        assertThat(post.getContent()).isEqualTo(expectedContent);
+    }
+
+    @Test
+    @WithMockUser(roles = "USER")
+    public void Post_삭제된다() throws Exception {
+        //given
+        Post savedPost = postRepository.save(new Post("title", "content", savedUser));
+        Long postId = savedPost.getId();
+
+        String url = "http://localhost:" + port + "/api/v1/posts/" + postId;
+
+        //when
+        mvc.perform(delete(url))
+                .andExpect(status().isOk());
+
+        //then
+        Optional<Post> result = postRepository.findById(postId);
+        assertThat(result.isEmpty()).isTrue();
+    }
 
 
 
